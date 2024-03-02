45151e0abfe06df79e2190fcf04e719f4a7f1fca
Wichukorn Dandecha <wichukorn.dandecha@gmail.com>
2024-03-02 15:45:34 +0700

    init unit test true

--- tests/unit/models/UserTest.php
+++ tests/unit/models/UserTest.php
@@ -2,11 +2,17 @@
 
 namespace tests\unit\models;
 
+use app\models\User;
+
 class UserTest extends \Codeception\Test\Unit
 {
     public function testFindUserById()
     {
-        User::findIdentity(1);
+        $user = User::findIdentity(1);
+        verify($user)->notNull();
+        verify($user->username)->equals('admin');
+        $user = User::findIdentity(null);
+        verify($user)->empty();
     }
     public function testFindUserByName()
     {
