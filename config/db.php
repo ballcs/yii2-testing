267195f539c064726cd2774426bbfca715c2eb17
Wichukorn Dandecha <wichukorn.dandecha@gmail.com>
2024-03-02 11:58:52 +0700

    init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=10.250.1.99;dbname=yii2-testing',
+    'username' => 'yii2-testing',
+    'password' => 'gmw2ymf-FKF4qhf9taz',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
